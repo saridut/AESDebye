d68132b63c6602f6173dede1369461f412806d98
Navid Panchi <navidpanchi@gmail.com>
2026-04-29 17:54:03 +0200

    reverted the profile ascii back

--- src/Profile.cc
+++ src/Profile.cc
@@ -1,6 +1,6 @@
 #include <Profile.hpp>
-// #include <asciichart/include/ascii/ascii.h>
-#include "../extern/asciichart/include/ascii/ascii.h"
+#include <asciichart/include/ascii/ascii.h>
+// #include "../extern/asciichart/include/ascii/ascii.h"
 
 
 Profile Profile::operator+(const Profile &rhs) const
